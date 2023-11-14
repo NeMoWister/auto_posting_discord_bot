9ce01dbd0e9520251462c4713b68d0ad8143cfc4
Ярослав <85142567+NeMoWister@users.noreply.github.com>
2023-11-14 13:34:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,8 @@
-token = 'OTU0OTE2NjkyNzI1NzYwMDYw.Go78KP.MAg69HfB49huSEb_zVOMboJCfebED5WmlLnLIs'
+token = ''
 
-anime_list = [('пять невест', 'D:/YandexDisk/Переезд/пять невест', 0.1, 650),
-              ('милый во франксе', 'D:/YandexDisk/Переезд/frankxx', 0.05, 50),
-              ('разное', 'D:/YandexDisk/Переезд/арты', 0.15, 500),
-              ('волчица и пряности', 'D:/YandexDisk/Переезд/Волчица', 0.05, 60),
-              ('среншин', 'D:/YandexDisk/Переезд/геншин', 0.1, 130),
-              ('кагуя', 'D:/YandexDisk/Переезд/кагуя', 0.1, 650),
-              ('кукла', 'D:/YandexDisk/Переезд/кукла', 0.1, 40),
-              ('фейтан', 'D:/YandexDisk/Переезд/фейт', 0.2, 1600),
-              ('тсукихиме', 'D:/YandexDisk/Переезд/тсукихиме', 0.1, 170),
-              ('глупый свин', 'D:/YandexDisk/Переезд/Глупый свин', 0.05, 250)]
+my_list = [(name1, path1, chance, MAX_CACHE_SIZE),
+              (name2, path2, chance, MAX_CACHE_SIZE)]
+# list = [('hello', 'c:/folder1', 0.5, 100),
+#          ('world'), 'c:/folder2', 0.5, 200]
 
-channel_id = 983601118590423080
-test_channel_id = 1173847623879376998
-
-
-test_anime_list = None
+channel_id = int
